5c83d98f35a43a301472364ef3871e5ccbe9c552
Nicholas Vilela <nvilela@proforma.com>
2020-03-03 10:25:55 -0500

    my stuff

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id='center'>
+    <div id='color-block' v-bind:style="{ backgroundColor: input }"></div>
+
+    <input id='input-field' type='text' autocomplete='off' placeholder='Enter some colors...' v-model='input' />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      input: ''
+    }
+  },
+
+  methods: {
+    changeColor: color => {
+      const redConditions    = color == 'red'
+      const blueConditions   = color == 'blue'
+      const yellowConditions = color == 'yellow'
+      const greenConditions  = color == 'green'
+      const brownConditions  = color == 'brown'
+      const orangeConditions = color == 'orange'
+
+      const displayColor = 
+          redConditions    ? 'red'
+        : blueConditions   ? 'blue'
+        : yellowConditions ? 'yellow'
+        : greenConditions  ? 'green'
+        : brownConditions  ? 'brown'
+        : orangeConditions ? 'orange' 
+        : ''
+
+      switch(color) {
+        case 'red':
+          return 'red'
+        case 'green':
+          return 'green'
+        case 'yellow':
+          return 'yellow'
+      }
+
+      return displayColor
+    }
   }
 }
 </script>
@@ -24,5 +55,25 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#color-block {
+    display: block;
+    height: 10rem;
+    width: 10rem;
+    border: solid black;
+}
+
+#center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+#input-field {
+  margin-top: 1rem;
+  height: 2rem;
+  width: 10rem;
 }
 </style>
